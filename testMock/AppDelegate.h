d43083fe26cd1a2c214582f2c6ef3d68df4ba214
yufei <850199472@example.com>
2015-04-07 14:41:13 +0800

    Initial Commit

---
+++ testMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testMock
+//
+//  Created by 喻飞 on 15/4/7.
+//  Copyright (c) 2015年 yuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testMock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testMock
+//
+//  Created by 喻飞 on 15/4/7.
+//  Copyright (c) 2015年 yuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
